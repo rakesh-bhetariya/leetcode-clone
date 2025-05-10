2fbc401a40cdc1d07d6693aac59b2526e239f299
Rakesh Bhetariya <rakeshbhetariya29@gmail.com>
2025-05-10 19:31:08 +0530

    First Commit
    Created project folder and initialized the project
    
    Installed Express.js for the backend server
    
    Installed dotenv for environment variable support

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
