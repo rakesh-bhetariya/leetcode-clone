2c6b7a33e4381d976abfced0ee88f6b88331823c
Rakesh Bhetariya <rakeshbhetariya29@gmail.com>
2025-05-23 12:32:33 +0530

    add home page with content and beatiful gradiante

--- fronted/src/page/HomePage.jsx
+++ fronted/src/page/HomePage.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="mt-4 text-3xl">This is home page</h1>
+    <div className="min-h-screen flex flex-col items-center mt-14 px-4">
+      <div className="absolute top-16 left-0 w-1/3 h-1/3 bg-primary opacity-30 blur-3xl rounded-md bottom-9"></div>
+      <h1 className="text-4xl font-extrabold z-10 text-center">LeetLab</h1>
+      <p className="mt-4 text-center text-lg font-semibold text-gray-50 dark:text-gray-400 z-10">
+        A Platform Inspired by Leetcode which helps you to prepare for coding
+        interviews and helps you to improve your coding skills by solving coding
+        problems
+      </p>
     </div>
   );
 };
